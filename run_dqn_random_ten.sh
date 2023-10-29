aa15dd4ace822c160de0f64e12127984ab8cd6e5
anthonypoon12 <anthony@poons.us>
2023-10-29 15:00:22 -0400

    runs program, using EPOCTIME as seed. Creates new directory for each seed and creates output file.

---
+++ run_dqn_random_ten.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+for i in {1.10}
+do
+    CURRENTEPOCTIME=`date +"%s"`
+    mkdir seed${CURRENTEPOCTIME}
+    bash run_dqn.sh --random-seed=$(CURRENTEPOCTIME) > seed${CURRENTEPOCTIME}/output.txt
+done
