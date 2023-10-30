5dbde7d355568204c2ef7610b9371ae9cff54bbf
anthonypoon12 <anthony@poons.us>
2023-10-29 23:49:22 -0400

    fix for loop typo

--- slurm_dqn.sh
+++ slurm_dqn.sh
@@ -5,7 +5,7 @@
 #$output=$2
 
 # Run sbatch
-for i in range(1..10)
+for i in {1..10}
 do
     sbatch --job-name=DQN_test --mem=300G --partition=scu-gpu --gres=gpu:2 --mail-type=BEGIN,END,FAIL --mail-user=anp4047@med.cornell.edu --wrap="bash ~/StressDQN/run_rand_seed_dqn.sh $i"
 done
