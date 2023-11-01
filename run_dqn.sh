cc64fa84e14dd79c5cb4f30d2bec66e2dcdcf35d
Estrin <dje4001@med.cornell.edu>
2023-11-01 14:47:47 -0400

    added hypertuning branch

--- run_dqn.sh
+++ run_dqn.sh
@@ -4,10 +4,10 @@
 #
 
 #Activate correct conda environment
-source ~/.bashrc
+#source ~/.bashrc
 conda activate StressDQN
 module load cuda
 nvidia-smi
-python train_atari.py
+python train_atari.py --random-seed 53
 
 #python train_atari.py $hyperparameter1 $hyp2 $hyp3
