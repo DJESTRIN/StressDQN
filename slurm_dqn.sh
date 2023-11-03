2255ed93b7fa76de917de9aea54119817402295f
David Estrin <dje4001@cayuga-login1.localdomain>
2023-11-02 20:57:59 -0400

    added dependency to script

--- slurm_dqn.sh
+++ slurm_dqn.sh
@@ -8,4 +8,7 @@ do
     RANDOMSEED=$(($CURRENTEPOCTIME + $i))
     sbatch --job-name=DQN_test --mem=300G --partition=scu-gpu --gres=gpu:2 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn.sh $RANDOMSEED"
 done
+
+sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=DQN_test --wrap="bash change_ownership.sh"
+
 exit
