7c5ead125992d05ebde8e625f0eecff971a15454
anthonypoon12 <anthony@poons.us>
2023-11-02 20:37:36 -0400

    cleanup

--- run_dqn.sh
+++ run_dqn.sh
@@ -2,12 +2,12 @@
 #hylp1=$1
 #hyp2=$2
 #
-
+$input=$1
 #Activate correct conda environment
 #source ~/.bashrc
 conda activate StressDQN
 module load cuda
 nvidia-smi
-python train_atari.py --random-seed 53
+python train_atari.py --random-seed $input
 
 #python train_atari.py $hyperparameter1 $hyp2 $hyp3
--- slurm_dqn.sh
+++ slurm_dqn.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
 # Need to add this to train_atari.py
-#$input=$1
-#$output=$2
-
 # Run sbatch
 for i in {1..10}
 do
-    sbatch --job-name=DQN_test --mem=300G --partition=scu-gpu --gres=gpu:2 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash /athena/listonlab/scratch/anp4047/StressDQN/run_junk_dqn.sh $i"
+    CURRENTEPOCTIME=`date +%s`
+    RANDOMSEED=$(($CURRENTEPOCTIME + $i))
+    sbatch --job-name=DQN_test --mem=300G --partition=scu-gpu --gres=gpu:2 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn.sh $RANDOMSEED"
 done
 exit
-
-# 500 jobs total
-# seed ===> this can be changed based on time
-# LR ==>
-# BS ==>
-# DF ==>
-# RBS ==>
-# Steps ==>
-# LearningStarts ==>
-# EpislonGreedy==>
-#
-#
