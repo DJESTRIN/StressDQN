5fc39313cacb63dcbcfab42fe479ca14ad45c17b
David Estrin <dje4001@med.cornell.edu>
2023-11-03 12:13:28 -0400

    Added human agent benchmark code

--- grab_human_agent_data.py
+++ grab_human_agent_data.py
@@ -6,14 +6,21 @@ Created on Thu Nov  2 16:04:08 2023
 @author: dje4001
 """
 import gym
-from gym.utils.play2 import play,PlayPlot
+from gym.utils.play_custom import play
+import os
+directory_name='/athena/listonlab/scratch/anp4047/human_benchmark/'
+subjectname=input('What is your first name?')
+drop_folder=directory_name+subjectname+'/'
+os.mkdir(drop_folder)
+filename=drop_folder+'cumulative_rewards.csv'
+env=gym.make('SpaceInvaders-v0')
+env.env.game_difficulty=0
+play(env,filename,zoom=4)
 
-if __name__=='__main__':
-    env=gym.make('SpaceInvaders-v0')
-    play(env,zoom=4)
-    
-    
-    
-    
-    
-    
+
+
+
+####
+# add env.env.game_difficulty=1
+
+
