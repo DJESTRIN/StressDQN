ac816e54336ad752db318fa8e00905976aab123c
anthonypoon12 <anthony@poons.us>
2023-11-02 22:20:34 -0400

    fixed issue with outputdir in bash

--- run_dqn.sh
+++ run_dqn.sh
@@ -6,9 +6,14 @@ RANDOMSEED=$1
 OUTPUTDIR=$2
 #Activate correct conda environment
 #source ~/.bashrc
+
+if [ -n "$OUTPUTDIR" ]; then
+  OUTPUTDIR="--output-dir $OUTPUTDIR"
+fi
+
 conda activate StressDQN
 module load cuda
 nvidia-smi
-python train_atari.py --random-seed $RANDOMSEED --output-dir $OUTPUTDIR
+python train_atari.py --random-seed $RANDOMSEED $OUTPUTDIR
 
 #python train_atari.py $hyperparameter1 $hyp2 $hyp3
