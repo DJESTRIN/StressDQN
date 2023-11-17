bc892b7539d115ddfa9fb91522f1a50d7aec1eef
anthonypoon12 <anthony@poons.us>
2023-11-17 12:22:47 -0500

    30 agents, junk, random, and normal

--- slurm_dqn.sh
+++ slurm_dqn.sh
@@ -6,23 +6,22 @@ for i in {1..10}
 do
     CURRENTEPOCTIME=`date +%s`
     RANDOMSEED=$(($CURRENTEPOCTIME + $i))
-    sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn.sh NoJunk random_agent dif_test_on $RANDOMSEED $OUTPUTDIR"
+    sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn.sh NoJunk not_random_agent dif_test_off $RANDOMSEED $OUTPUTDIR"
 done
 
 for i in {1..10}
 do
     CURRENTEPOCTIME=`date +%s`
     RANDOMSEED=$(($CURRENTEPOCTIME + $i))
-    sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_$
+    sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn.sh junk not_random_agent dif_test_off $RANDOMSEED $OUTPUTDIR"
 done
 
 for i in {1..10}
 do
     CURRENTEPOCTIME=`date +%s`
     RANDOMSEED=$(($CURRENTEPOCTIME + $i))
-    sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_$
+    sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn.sh NoJunk random_agent dif_test_off $RANDOMSEED $OUTPUTDIR"
 done
-
 
 sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=DQN_test --wrap="bash change_ownership.sh"
 
