46950024ee7f5088b31bf11d6abcf1912a7355a7
anthonypoon12 <anthony@poons.us>
2024-01-17 13:20:46 -0500

    adding files to help testing

---
+++ run_dqn_Anthony.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+JUNK=$1
+RANDOM_AGENT=$2
+DIF_TEST=$3
+RANDOMSEED=$4
+OUTPUTDIR=$5
+LR=$6
+BS=$7
+
+#Determine type of run
+if [ "$JUNK" == "junk" ]; then
+  JUNK="--junk"
+else
+  JUNK=""
+fi
+
+if [ "$RANDOM_AGENT" == "random_agent" ]; then
+  RANDOM_AGENT="--random-choice"
+else
+  RANDOM_AGENT=""
+fi
+
+if [ "$DIF_TEST" == "dif_test_on" ]; then
+  DIF_TEST="--difficulty-test"
+else
+  DIF_TEST=""
+fi
+
+if [ -n "$OUTPUTDIR" ]; then
+  OUTPUTDIR="--output-dir $OUTPUTDIR"
+fi
+
+#Execute python command
+source ~/.bashrc
+conda activate ~/anaconda3/envs/StressDQN
+module load cuda
+nvidia-smi
+python train_atari.py --random-seed $RANDOMSEED $OUTPUTDIR $JUNK $RANDOM_AGENT $DIF_TEST --num-steps 100000 --learning-starts 1000
