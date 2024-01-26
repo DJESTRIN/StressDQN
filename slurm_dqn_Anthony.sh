9c44a828015ae36d08b957d714af2effee385972
David Estrin <dje4001@med.cornell.edu>
2024-01-26 15:47:30 -0500

    update anthony code with reservation

--- slurm_dqn_Anthony.sh
+++ slurm_dqn_Anthony.sh
@@ -4,7 +4,7 @@ OUTPUTDIR=$1 #real
 # Run sbatch
 CURRENTEPOCTIME=`date +%s`
 RANDOMSEED=$(($CURRENTEPOCTIME + $i))
-sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn_Anthony.sh NoJunk not_random_agent dif_test_off $RANDOMSEED $OUTPUTDIR"
+sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --gres=gpu:1 --reservation=listonlab --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn_Anthony.sh NoJunk not_random_agent dif_test_off $RANDOMSEED $OUTPUTDIR"
 
 sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=DQN_test --wrap="bash change_ownership.sh"
 
