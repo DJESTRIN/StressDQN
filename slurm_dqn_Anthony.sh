3244c3cabcc60b1a51cb8a6d64a10d56b7a2dc1d
anthonypoon12 <anthony@poons.us>
2024-01-31 09:40:23 -0500

    remove unnecessary reservation

--- slurm_dqn_Anthony.sh
+++ slurm_dqn_Anthony.sh
@@ -6,7 +6,7 @@ CURRENTEPOCTIME=`date +%s`
 RANDOMSEED=$(($CURRENTEPOCTIME))
 sbatch --job-name=DQN_test --mem=100G --partition=scu-gpu --reservation=listonlab --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu,anp4047@med.cornell.edu --wrap="bash run_dqn_Anthony.sh NoJunk not_random_agent dif_test_off $RANDOMSEED $OUTPUTDIR"
 
-sbatch --mem=5G --partition=scu-cpu --reservation=listonlab --dependency=singleton --job-name=DQN_test --wrap="bash change_ownership.sh"
+sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=DQN_test --wrap="bash change_ownership.sh"
 
 exit
 
